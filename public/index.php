1820036b849678347335436ab2a93a88b50de40c
Nabil Mecili <nabilmecili@gmail.com>
2022-05-24 17:47:57 +0200

    from 33 to 9 Deprecations

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 use App\Kernel;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
+use Symfony\Component\ErrorHandler\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
