cfc48abaab8e5d614ca6f7d3eb01dc246712cecb
Nabil Mecili <nabilmecili@gmail.com>
2022-06-19 14:01:49 +0200

    from 39 to 19 depricated

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,11 +12,14 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="login")
      */
-    public function loginAction(AuthenticationUtils $authenticationUtils,Request $request)
+    public function loginAction(AuthenticationUtils $authenticationUtils): Response
     {
         // $authenticationUtils = $this->container->get('security.authentication_utils');
 
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', array(
