eeed251da06547b0d6d8eea596d9a1c7eef6a816
Nabil Mecili <nabilmecili@gmail.com>
2022-06-22 17:01:24 +0200

    installation of "fixtures"

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,71 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Task;
+use App\Entity\User;
+use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+    
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        $admin = new User();
+
+        $admin->setEmail('admin@admin.fr');
+        $admin->setUsername($faker->lastName());
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'Azerty1+'));
+
+        $manager->persist($admin);
+        $this->addReference('admin', $admin);
+
+        // Ajouter 3 taches pour l'admin
+        for($i=0; $i<3; $i++) {
+            $task = new Task();
+
+            $task->setTitle($faker->realtext(10, 2));
+            $task->setContent($faker->realtext(70, 2));
+            $task->setCreatedAt($faker->dateTimeBetween('-1 month', 'now'));
+            $task->setAuthor($admin);
+            $manager->persist($task);
+        }
+
+        for($i=0; $i<5; $i++) {
+            $user = new User();
+
+            $user->setEmail($faker->email());
+            $user->setUsername($faker->lastName());
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($this->passwordHasher->hashPassword($admin, 'Azerty1+'));
+            $manager->persist($user);
+
+            // Ajouter 3 taches pour chaque user
+            for($k=0; $k<3; $k++) {
+                $task = new Task();
+
+                $task->setTitle($faker->realtext(10, 2));
+                $task->setContent($faker->realtext(70, 2));
+                $task->setCreatedAt($faker->dateTimeBetween('-1 month', 'now'));
+                $task->setAuthor($user);
+                $manager->persist($task);
+        }
+
+
+        }
 
         $manager->flush();
+
     }
 }
