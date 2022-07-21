452e15b3c8c0f0de31e8c19c80e82cf8ca2aad08
Nabil Mecili <nabilmecili@gmail.com>
2022-07-21 10:38:30 +0200

    Add UserVoterTest

---
+++ tests/Security/UserVoterTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Security\Voter;
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+use App\Security\Voter\TaskVoter;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class UserVoterTest extends TestCase
+{
+    public function testVoteOnAttributeFalse()
+    {
+        $tokenInterface = $this->getMockBuilder(TokenInterface::class)->disableOriginalConstructor()->getMock();
+        $security = $this->getMockBuilder(Security::class)->disableOriginalConstructor()->getMock();
+        $voter = new TaskVoter($security);
+        $this->assertEquals(0, $voter->vote($tokenInterface, (new User()), ["WRONG_ATTRIBUTE"]));
+    }
+}
